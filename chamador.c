5b3d95c233c1b765f33fcabae6069c2c08dd30f5
naiche <naicheab@gmail.com>
2013-12-09 19:10:52 -0200

    Update chamador.c

--- chamador.c
+++ chamador.c
@@ -88,38 +88,36 @@ int main()
 	
 	if (t2fs_first (&dir))
 	{
-	printf ("Erro ao abrir o diretorio.\n");
-	return;
+		printf ("Erro ao abrir o diretorio.\n");
+		return;
 	}
 
 	char *name = malloc(sizeof(rec.name)+1);
 	if (name==NULL)
 	{
-	printf ("Erro ao ler o diretorio.\n");
-	return;
+		printf ("Erro ao ler o diretorio.\n");
+		return;
 	}
 	
 	count=0;
 	while ( (i=t2fs_next(&dir, &rec)) == 0 )
 	{
-	printf ("%s\n", (char *)rec.name);
-	count++;
+		printf ("%s\n", (char *)rec.name);
+		count++;
 	}
 	
 	free(name);
 	
 	if (i!=1)
 	{
-	printf ("Erro ao ler o diretorio.\n");
-	return;
+		printf ("Erro ao ler o diretorio.\n");
+		return;
 	}
 	if (count==0)
 	printf ("Diretorio vazio.\n");
 	else
 	printf ("\nEncontrados %d arquivos.\n\n", count);
 	sair();
-	return 0;
-
 
 	t2fs_delete("Arquivo1");
 	retorno = t2fs_open("Arquivo1");
